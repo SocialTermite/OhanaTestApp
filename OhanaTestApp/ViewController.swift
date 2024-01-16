96fabd5e56572cdca216d1b87dd1ab4e462e487e
Konstantin Bondar <bondar.k.dev@gmail.com>
2024-01-16 12:02:42 +0100

    Initial Commit

---
+++ OhanaTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OhanaTestApp
+//
+//  Created by Konstantin Bondar on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
